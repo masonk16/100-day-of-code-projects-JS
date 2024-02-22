3810c78c7b49ade2a94082f900ca05033be0c9e6
Mason Mapfundematsva <masonmapfunde@gmail.com>
2024-02-22 21:17:11 +0200

    Day - 2 variables and data types

---
+++ day-2/script.js
@@ -0,0 +1,26 @@
+"use strict"
+
+// Number
+let n = 390
+n = 3.8797
+
+// BigInt
+let x = 6555452856325566232154231542158452184521456854214541n
+
+// String
+let zvinhu = "Souljah Love"
+let new_str = `Ndine maDollar ${n}`
+
+// Boolean
+let tichaita_mari = true
+
+// Null value
+let age = null
+
+// undefined value
+let mhanya
+
+// Objects and Symbols
+
+
+
