783ae82f1138bc326baafddbe30e098b236bd902
Mason Mapfundematsva <masonmapfunde@gmail.com>
2024-02-27 20:34:24 +0200

    Day 3 - type checking and operators

---
+++ day-3/operators.js
@@ -0,0 +1,10 @@
+"use strict"
+
+alert(21 > 23)
+alert(34 < 45)
+alert(12 == 78)
+alert(45 != 56)
+alert("Camping" > "Camp")
+alert("15">10)
+alert(false==0)
+alert(false===0) // strict equality check
---
+++ day-3/typechec.js
@@ -0,0 +1,11 @@
+"use strict"
+
+let n = "jenjere"
+
+alert(typeof true)
+alert(typeof 987576)
+alert(typeof "Ngatipushe")
+alert(typeof undefined)
+alert(typeof null)
+alert(typeof Math)
+alert(typeof `Hustle toripusha kunge ${n}`)
