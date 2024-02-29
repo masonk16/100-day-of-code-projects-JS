c47ea3d8a168e7f78f2d3072e7d7c88598676556
Mason Mapfundematsva <masonmapfunde@gmail.com>
2024-02-29 21:07:34 +0200

    Day 4 - assignment 2

---
+++ day-4/assignment-2/script.js
@@ -0,0 +1,27 @@
+"use strict"
+
+// Declare two variables: admin and name.
+// Assign the value "John" to name.
+// Copy the value from name to admin.
+// Show the value of admin using alert (must output “John”).
+let name = "John";
+let admin = name;
+alert(admin)
+
+// Create a variable with the name of our planet. How would you name such a variable?
+// Create a variable to store the name of a current visitor to a website. How would you name that variable?
+let planet_name = "Earth";
+let current_visitor = null;
+
+// What is the output of:
+
+let name = "Ilya";
+
+alert( `hello ${1}` ); // ?
+// hello 1
+
+alert( `hello ${"name"}` ); // ?
+// hello name
+
+alert( `hello ${name}` ); // ?
+// hello Ilya
