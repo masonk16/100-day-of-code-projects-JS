03c4da41624ddf99acdf8d2b491c6580a2e19082
Mason Mapfundematsva <masonmapfunde@gmail.com>
2024-03-05 08:28:48 +0200

    Day 6 - Logical operators

---
+++ day-6/script.js
@@ -0,0 +1,21 @@
+"use strict"
+
+/* Logical operators
+OR - ||
+AND - &&
+NOT - !
+Nullish Coalescing - ??
+ */
+
+let a = 12
+let b = 23
+let c = 56
+
+if (a==12 || b==23 || c==56){
+    alert("True")
+} else {
+    alert("False!")
+}
+
+let result = confirm("Whay a u gehh?")
+alert(result)
